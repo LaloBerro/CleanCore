8886fc8488c64e83b7f3eb0151829e917476e28d
Lalo <laurencioberro@gmail.com>
2021-09-28 12:15:57 -0300

    [A] Add BaseMain to CleanArchitectureBases

---
+++ Runtime/CleanArchitectureBases/Main/BaseMain.cs
@@ -0,0 +1,20 @@
+﻿using CleanCore.CleanArchitectureBases.Domain.UseCases;
+using UnityEngine;
+
+namespace CleanCore.CleanArchitectureBases.Main
+{
+    public abstract class BaseMain : MonoBehaviour, IMain
+    {
+        protected IUseCase _useCase;
+
+        private void Awake()
+        {
+            _useCase.Begin();
+        }
+
+        private void OnDestroy()
+        {
+            _useCase.Finish();
+        }
+    }
+}
