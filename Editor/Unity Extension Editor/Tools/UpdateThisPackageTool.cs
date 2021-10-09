bb72fa5602884cd674ae7db42bc2fab65e14778b
Lalo <laurencioberro@gmail.com>
2021-10-09 18:20:07 -0300

    [A] Add Editor bar button to update this package

---
+++ Editor/Unity Extension Editor/Tools/UpdateThisPackageTool.cs
@@ -0,0 +1,33 @@
+using UnityEditor;
+using UnityEditor.PackageManager.Requests;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+namespace CleanCore.EditorExtensions.CustomTools
+{
+	public class UpdateThisPackageTool : Editor
+	{
+        private static AddRequest Request;
+
+        [MenuItem("Custom Editor/Update Clean Core package",priority = 0)]
+        public static void Add()
+        {
+            Debug.Log("Updating Clean Core");
+            Request = Client.Add("https://github.com/LaloBerro/CleanCore.git");
+            EditorApplication.update += Progress;
+        }
+
+        private static void Progress()
+        {
+            if (Request.IsCompleted)
+            {
+                if (Request.Status == StatusCode.Success)
+                    Debug.Log("Installed: " + Request.Result.packageId);
+                else if (Request.Status >= StatusCode.Failure)
+                    Debug.Log(Request.Error.message);
+
+                EditorApplication.update -= Progress;
+            }
+        }
+    }
+}
