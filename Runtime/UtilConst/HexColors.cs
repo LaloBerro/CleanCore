e8c12e4e993f704335ce74cef70f00730abb25f8
Lalo <laurencioberro@gmail.com>
2021-10-26 12:48:58 -0300

    [A] Add utils Const (icons and HexColors)

---
+++ Runtime/UtilConst/HexColors.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanCore.UtilsConst
+{
+    public class HexColors
+    {
+        public const string Orange = "#f4a261";
+        public const string Green = "#2a9d8f";
+        public const string LightBlue = "#00b4d8";
+        public const string Violet = "#7209b7";
+        public const string RedPink = "#e63946";
+    }
+}
---
+++ Runtime/UtilConst/Icons.cs
@@ -0,0 +1,17 @@
+﻿using CleanCore.Extensions;
+
+namespace CleanCore.UtilsConst
+{
+    public class Icons
+    {
+        public const string Plus = "＋";
+        public const string RightArrow = "→";
+        public const string Check = "✓";
+        public const string Cross = "╳";
+
+        public static string Format(string icon, string hexColor)
+        {
+            return icon.SetColorOfString(hexColor).MakeBold();
+        }
+    }
+}
