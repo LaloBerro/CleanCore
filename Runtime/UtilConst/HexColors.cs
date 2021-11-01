81e253677c1cb3c5196170208096d70238dcbbd6
Lalo <Laurencioberro@gmail.com>
2021-11-01 17:27:37 -0300

    [A] Add new Colors

--- Runtime/UtilConst/HexColors.cs
+++ Runtime/UtilConst/HexColors.cs
@@ -4,8 +4,12 @@
     {
         public const string Orange = "#f4a261";
         public const string Green = "#2a9d8f";
+        public const string LightGreen = "#80ed99";
         public const string LightBlue = "#00b4d8";
         public const string Violet = "#7209b7";
         public const string RedPink = "#e63946";
+        public const string YellowGreen = "#ccff33";
+        public const string YellowOrange = "#ee9b00";
+        public const string Yellow = "#ffd60a";
     }
 }
