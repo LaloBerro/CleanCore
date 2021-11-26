6643ff345138ee16e632d4a66569ec5fe133a97f
Lalo <Laurencioberro@gmail.com>
2021-11-26 13:28:50 -0300

    [F] Fix sceneDatas

--- Runtime/Scenes Controller/SceneData.cs
+++ Runtime/Scenes Controller/SceneData.cs
@@ -1,40 +1,26 @@
-﻿using UnityEngine;
-
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
+﻿using System;
+using UnityEngine;
 
 namespace CleanCore.Scenes
 {
     [System.Serializable]
     public class SceneData
     {
-
-#if UNITY_EDITOR
-        [System.NonSerialized]
-        public SceneAsset sceneAsset;
-#endif
-
-        [Header("Config")]
+        public string nameScene;
         public bool useLoadingScreen = true;
         public bool isLockedScene;
         public bool removeLockedScenes;
         public bool isPrincipal;
 
-        public SceneData[] scenesData;
-
-        public string zoneName;
-        public string nameScene;
+        public SceneDataSO[] scenesData;
 
         public SceneData[] GetAllScenesToOpen()
         {
             SceneData[] scenes = new SceneData[scenesData.Length + 1];
 
-            scenes[scenesData.Length] = this;
-
             for (int i = 0; i < scenesData.Length; i++)
             {
-                scenes[i] = scenesData[i];
+                scenes[i] = scenesData[i].SceneData;
             }
 
             return scenes;
